9b9e8854d60b632d4022b49db6fb4c4924c8b2ef
Jeremiah Lena <101596542+jeremiahFI@users.noreply.github.com>
2022-11-05 13:56:30 +0100

    lil changes

--- App.tsx
+++ App.tsx
@@ -1,63 +1,65 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, useColorScheme, Alert } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack';
-import { RouteNames } from './constants';
-import { Provider } from 'react-redux';
+import React, {useEffect} from 'react';
+import {useColorScheme} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {RouteNames} from './constants';
+import {Provider} from 'react-redux';
 import SplashScreen from 'react-native-splash-screen';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-
 /* SCREENS */
-import { AboutMe, News, StoryWebView, AuthScreen } from './screens';
+import {AboutMe, News, StoryWebView, AuthScreen} from './screens';
 import store from './store';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createTable, deleteTable, getCurrentUser, getDBConnection, TABLE_NAMES } from './sqlite3/index.database';
-import { SQLiteDatabase } from 'react-native-sqlite-storage';
-
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 function BottomTabs() {
-
-  const isDarkMode = useColorScheme() === 'dark'
-  const Tabs = createBottomTabNavigator()
+  const isDarkMode = useColorScheme() === 'dark';
+  const Tabs = createBottomTabNavigator();
   return (
     <Tabs.Navigator
       screenOptions={{
         headerShown: false,
         tabBarShowLabel: false,
         tabBarStyle: {
-          backgroundColor: isDarkMode ? 'black' : 'white'
+          backgroundColor: isDarkMode ? 'black' : 'white',
         },
-      }}
-    >
+      }}>
       <Tabs.Screen
-        name='homeTab'
+        name="homeTab"
         component={News}
         options={{
-          tabBarIcon: ({ focused }) => <FontAwesome name='hacker-news' color={focused ? '#ec6333' : 'gray'} size={30} />
+          tabBarIcon: ({focused}) => (
+            <FontAwesome
+              name="hacker-news"
+              color={focused ? '#ec6333' : 'gray'}
+              size={30}
+            />
+          ),
         }}
       />
       <Tabs.Screen
-        name='aboutTab'
+        name="aboutTab"
         component={AboutMe}
         options={{
-          tabBarIcon: ({ focused }) => <FontAwesome name='user' color={focused ? '#ec6333' : 'gray'} size={28} />
+          tabBarIcon: ({focused}) => (
+            <FontAwesome
+              name="user"
+              color={focused ? '#ec6333' : 'gray'}
+              size={28}
+            />
+          ),
         }}
       />
     </Tabs.Navigator>
-  )
+  );
 }
 
-
 const Component: React.FC = () => {
-
-  const [db, setDb] = useState<SQLiteDatabase>();
-
   useEffect(() => {
     SplashScreen.hide();
-  }, [])
+  }, []);
 
-  const Stack = createStackNavigator()
+  const Stack = createStackNavigator();
 
   return (
     <Provider store={store}>
@@ -66,31 +68,20 @@ const Component: React.FC = () => {
           initialRouteName={RouteNames.authScreen}
           screenOptions={{
             headerShown: false,
-          }}
-        >
-          <Stack.Screen
-            name={RouteNames.newsScreen}
-            component={BottomTabs}
-          />
+          }}>
+          <Stack.Screen name={RouteNames.newsScreen} component={BottomTabs} />
 
-          <Stack.Screen
-            name={RouteNames.aboutScreen}
-            component={AboutMe}
-          />
+          <Stack.Screen name={RouteNames.aboutScreen} component={AboutMe} />
 
           <Stack.Screen
             name={RouteNames.storyScreen}
             component={StoryWebView}
           />
-          <Stack.Screen
-            name={RouteNames.authScreen}
-            component = {AuthScreen}
-          />
+          <Stack.Screen name={RouteNames.authScreen} component={AuthScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
-  )
-}
+  );
+};
 
-
-export default Component
+export default Component;
--- constants.ts
+++ constants.ts
@@ -1,10 +1,10 @@
-const HACKERNEWS_API : string = 'https://hacker-news.firebaseio.com/v0';
+const HACKERNEWS_API: string = 'https://hacker-news.firebaseio.com/v0';
 
-const RouteNames  = {
-    newsScreen: 'News Screen',
-    aboutScreen: 'About Screen',
-    storyScreen: 'Story Screen',
-    authScreen: 'Login Screen',
-}
+const RouteNames = {
+  newsScreen: 'News Screen',
+  aboutScreen: 'About Screen',
+  storyScreen: 'Story Screen',
+  authScreen: 'Login Screen',
+};
 
-export {HACKERNEWS_API, RouteNames}
+export {HACKERNEWS_API, RouteNames};
